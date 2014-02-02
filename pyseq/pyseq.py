50f58d68caaebca995c140b12bc163777278fb60
dparks1134 <donovan.parks@gmail.com>
2014-02-03 09:20:57 +1000

    Added genomicSignature class and added functionality to Fastx class.

--- pyseq/pyseq.py
+++ pyseq/pyseq.py
@@ -5,7 +5,7 @@
 #                                                                             #
 #    Implements a bunch of operations commonly done to bio sequences          #
 #                                                                             #
-#    Copyright (C) Michael Imelfort                                           #
+#    Copyright (C) Michael Imelfort and Donovan Parks                         #
 #                                                                             #
 ###############################################################################
 #                                                                             #
@@ -24,9 +24,9 @@
 #                                                                             #
 ###############################################################################
 
-__author__ = "Michael Imelfort"
+__author__ = "Michael Imelfort, Donovan Parks"
 __copyright__ = "Copyright 2013"
-__credits__ = ["Michael Imelfort"]
+__credits__ = ["Michael Imelfort", "Donovan Parks"]
 __license__ = "GPLv3"
 __version__ = "0.0.1"
 __maintainer__ = "Michael Imelfort"
@@ -35,26 +35,46 @@ __status__ = "Dev"
 
 ###############################################################################
 ###############################################################################
-###############################################################################
-###############################################################################
 
 import string
+import logging
 
-###############################################################################
-###############################################################################
 ###############################################################################
 ###############################################################################
 
 class Fastx():
     """Class for manipulating fasta sequences"""
     def __init__(self):
+        self.logger = logging.getLogger()
         self.complements = string.maketrans('acgtrymkbdhvACGTRYMKBDHV', 'tgcayrkmvhdbTGCAYRKMVHDB')
 
     def revComp(self, seq):
         """reverse complement a sequence"""
         return seq.translate(self.complements)[::-1]
+    
+    def lexicographicallyLowest(self, seq):
+        """return the lexicographically lowest form of a sequence"""
+        rseq = self.revComp(seq)
+        if(seq < rseq):
+            return seq
+        return rseq
+    
+    def baseCount(self, seq):
+        """number of A, C, G, and T (U) bases in sequence"""
+        temp_seq = seq.upper()
+        
+        a = temp_seq.count('A')
+        c = temp_seq.count('C')
+        g = temp_seq.count('G') 
+        t = temp_seq.count('T') + temp_seq.count('U')
+        
+        return a, c, g, t
+        
+    def gc(self, seq):
+        """percentage of sequence consisting of G or C"""
+        a, c, g, t = self.baseCount(seq)
+        
+        return float(g + c) / (a + c + g + t)
 
-###############################################################################
-###############################################################################
 ###############################################################################
 ###############################################################################
